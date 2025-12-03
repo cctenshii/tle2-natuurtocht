f072f43fa4530dd1df227050f0e571774514a275
cctenshii <SissiWang2003@gmail.com>
2025-12-03 16:00:40 +0100

    ipv een card model, gebruik ik nu de natureitem model

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Card;
+use App\Models\NatureItem;
 use Illuminate\Http\Request;
 
 class CardController extends Controller
 {
-    public function show(Card $card)
+    public function show(NatureItem $card)
     {
         return view('cards.show', [
             'card' => $card
