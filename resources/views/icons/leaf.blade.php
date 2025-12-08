8e7ca1947e734385dc263bb39a7b22bd3b88286e
ThijsVanLoo1 <thijs30042002@gmail.com>
2025-12-08 11:38:32 +0100

    Added automatic seasoning based on current month on index.blade.php Season styles are added to text with each changing season

---
+++ resources/views/icons/leaf.blade.php
@@ -0,0 +1,5 @@
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-5 h-5">
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+          d="M12 2c4 2 7 6 6 10-1 4-5 7-9 6l-3 3 1-4c-3-3-3-8 0-11 2-2 5-3 8-4z"/>
+</svg>
+
