272e48d4727bac53fecb31d7a5dc1ba05afe18bf
neotroenopawiro <302869724@student.rocmondriaan.nl>
2025-12-08 13:27:34 +0100

    added copy invite link

--- resources/views/natuur-dex/index.blade.php
+++ resources/views/natuur-dex/index.blade.php
@@ -92,6 +92,8 @@
         <!-- 3. Vaste Footer -->
         <footer class="absolute bottom-0 left-0 right-0 bg-cyan-800 text-white z-10">
             <div class="flex justify-between items-center p-4">
+
+                <!-- Profiel -->
                 <a href="#" class="flex flex-col items-center gap-1 text-sm font-semibold">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-6 h-6">
                         <path fill-rule="evenodd"
@@ -100,10 +102,24 @@
                     </svg>
                     <span>Profiel</span>
                 </a>
+
+                <!-- Invite (kopieer link) -->
+                <button
+                    type="button"
+                    id="inviteBtn"
+                    class="flex flex-col items-center gap-1 text-sm font-semibold focus:outline-none"
+                >
+                    <!-- users/invite icoon -->
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-7 h-7">
+                        <path d="M16 11c1.66 0 2.99-1.57 2.99-3.5S17.66 4 16 4s-3 1.57-3 3.5S14.34 11 16 11Zm-8 0c1.66 0 2.99-1.57 2.99-3.5S9.66 4 8 4 5 5.57 5 7.5 6.34 11 8 11Zm0 2c-2.33 0-7 1.17-7 3.5V20h14v-3.5C15 14.17 10.33 13 8 13Zm8 0c-.29 0-.62.02-.97.05 1.16.84 1.97 1.97 1.97 3.45V20h7v-3.5c0-2.33-4.67-3.5-7-3.5Z"/>
+                    </svg>
+                    <span>Invite</span>
+                </button>
+
+                <!-- Collectie -->
                 <a href="#" class="flex flex-col items-center gap-1 text-sm font-semibold">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-6 h-6">
-                        <path
-                            d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z"/>
+                        <path d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z"/>
                         <path fill-rule="evenodd"
                               d="M8.25 2.25a.75.75 0 01.75.75v.5a.75.75 0 01-1.5 0v-.5a.75.75 0 01.75-.75zM10.75 2.25a.75.75 0 01.75.75v.5a.75.75 0 01-1.5 0v-.5a.75.75 0 01.75-.75zM10 5.25a.75.75 0 00-1.5 0v4.5a.75.75 0 001.5 0v-4.5z"
                               clip-rule="evenodd"/>
@@ -113,7 +129,41 @@
                     </svg>
                     <span>Collectie</span>
                 </a>
+
             </div>
+
+            <!-- kleine toast -->
+            <div id="inviteToast" class="hidden absolute bottom-16 left-1/2 -translate-x-1/2 bg-black/70 text-white text-xs px-3 py-2 rounded">
+                Link gekopieerd!
+            </div>
+
+            <script>
+                (function () {
+                    const btn = document.getElementById('inviteBtn');
+                    const toast = document.getElementById('inviteToast');
+
+                    // Zet hier jouw App Store / Play Store link
+                    const INVITE_LINK = "betalen kankerboef!!"; // <-- vervang
+
+                    btn?.addEventListener('click', async () => {
+                        try {
+                            await navigator.clipboard.writeText(INVITE_LINK);
+                        } catch (e) {
+                            // fallback voor oudere browsers
+                            const tmp = document.createElement('input');
+                            tmp.value = INVITE_LINK;
+                            document.body.appendChild(tmp);
+                            tmp.select();
+                            document.execCommand('copy');
+                            document.body.removeChild(tmp);
+                        }
+
+                        toast.classList.remove('hidden');
+                        setTimeout(() => toast.classList.add('hidden'), 1400);
+                    });
+                })();
+            </script>
         </footer>
+
     </div>
 </x-app-layout>
